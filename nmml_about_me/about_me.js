6869b4a7d00e3ec1c8575dd2b943efd525a346a5
Nicholas Leoutsakos <nleoutsa@gmail.com>
2015-03-01 12:29:07 -0500

    adding nleoutsa's about_me files

---
+++ nmml_about_me/about_me.js
@@ -0,0 +1,64 @@
+// about_me.js
+
+// array of images
+var images = [
+    "url('http://i.imgur.com/wiu9cFk.jpg')", // horse
+    "url('http://i.imgur.com/TJ7YEjs.jpg')", // green sweatshirt
+    "url('http://i.imgur.com/TFkftVq.jpg')", // Lady Agnew
+    "url('http://i.imgur.com/xnv5NqD.jpg')", // emma sitting
+    "url('http://i.imgur.com/09m22VZ.jpg')", // track
+    "url('http://i.imgur.com/YiVlgnF.jpg')", // emma yellow
+    "url('http://i.imgur.com/ga60NEv.jpg')", // first self portrait
+    "url('http://i.imgur.com/EQOvgXu.jpg')", // elephants
+    "url('http://i.imgur.com/Pzz6Ded.jpg')", // nose ring
+    "url('http://i.imgur.com/p0Mp7EM.jpg')", // sailor
+    "url('http://i.imgur.com/76xH6od.jpg')", // reclining
+    "url('http://i.imgur.com/kutJzmS.jpg')"	// the stare
+];
+// what is the current image?
+var currentImage = 0;
+//clicking left and right pics(or big pic) shuffles through image array
+$('.leftPic').click(function() {
+    currentImage = currentImage - 1;
+    if (currentImage < 0) {
+        currentImage = images.length -1;
+    };
+    loopImages();
+});
+$('.rightPic').click(function(){
+    currentImage = currentImage + 1;
+    if (currentImage > images.length -1) {
+        currentImage = 0;
+    };
+    loopImages();
+});
+$('.big').click(function(){
+    currentImage = currentImage + 1;
+    if (currentImage > images.length -1) {
+        currentImage = 0;
+    };
+    loopImages();
+});
+// clicking center image expands/collapses pics
+$('.photos').click(function() {
+    $("div").toggleClass('expand');
+    $(".big").toggleClass('zoom');
+    $(".info").toggleClass('lower');
+});
+//display images in a loop
+var loopImages = function() {
+    $('.leftPic').css("background", images[currentImage - 1]);
+    $('.photos').css("background", images[currentImage]);
+    $('.rightPic').css("background", images[currentImage + 1]);
+    $('.big').css("background", images[currentImage]);
+    if (currentImage == 0) {
+        $('.leftPic').css("background", images[images.length - 1]);
+    }
+    else if (currentImage == images.length - 1) {
+        $('.rightPic').css("background", images[0]);
+    }
+};
+// start with images collapsing
+$("div").toggleClass('expand');
+// set images
+loopImages();
